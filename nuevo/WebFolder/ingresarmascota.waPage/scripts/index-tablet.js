11a57c8d65d1fea52fc53ca703994ae002485b9f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-07-07 03:17:46 +0200

    Update #40

--- nuevo/WebFolder/ingresarmascota.waPage/scripts/index-tablet.js
+++ nuevo/WebFolder/ingresarmascota.waPage/scripts/index-tablet.js
@@ -2,10 +2,16 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
 	var button2 = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		alert('Ingrese los datos en el formulario!');
+	};// @lock
 
 	button2.click = function button2_click (event)// @startlock
 	{// @endlock
@@ -23,6 +29,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
 	WAF.addListener("button2", "click", button2.click, "WAF");
 // @endregion
 };// @endlock
--- nuevo/WebFolder/ingresarmascota.waPage/scripts/index.js
+++ nuevo/WebFolder/ingresarmascota.waPage/scripts/index.js
@@ -2,10 +2,16 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button5 = {};	// @button
 	var button6 = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button5.click = function button5_click (event)// @startlock
+	{// @endlock
+		alert('Ingrese los datos en el formulario!');
+	};// @lock
 
 	button6.click = function button6_click (event)// @startlock
 	{// @endlock
@@ -22,6 +28,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button5", "click", button5.click, "WAF");
 	WAF.addListener("button6", "click", button6.click, "WAF");
 // @endregion
 };// @endlock
